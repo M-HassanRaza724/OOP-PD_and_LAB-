96956cc8c332122d41446eab35ee202e3e30c60c
M-HassanRaza724 <2024cs213@student.uet.edu.pk>
2025-04-14 16:58:54 +0500

    [long commit]

---
+++ week8/PD8/CardGame/BL/BlackjackHand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame.BL
+{
+    class BlackjackHand : Hand
+    {
+        public BlackjackHand()
+        {
+            Cards = new List<Card>();
+        }
+        public int GetValue()
+        {
+            int value = 0;
+            int aces = 0;
+            foreach (Card card in Cards)
+            {
+                if (card.getValue() == 1)
+                {
+                    aces++;
+                    value += 11;
+                }
+                else if (card.getValue() >= 10)
+                {
+                    value += 10;
+                }
+                else
+                {
+                    value += card.getValue();
+                }
+            }
+            // Adjust for Aces if value exceeds 21
+            while (value > 21 && aces > 0)
+            {
+                value -= 10;
+                aces--;
+            }
+            return value;
+        }
+    }
+}
---
+++ week8/PD8/CardGame/BL/Hand.cs
@@ -0,0 +1,51 @@
+﻿namespace CardGame.BL
+{
+    class Hand
+    {
+        protected List<Card> Cards;
+        public Hand()
+        {
+            Cards = new List<Card>();
+        }
+
+        public void clear()
+        {
+            Cards.Clear();
+        }
+
+        public void AddCard(Card c)
+        {
+           Cards.Add(c);
+        }
+        public void RemoveCard(Card c)
+        {
+           Cards.Remove(c);
+        }
+        public int GetCardCount()
+        {
+          return Cards.Count();
+        }
+        public void removeCard(int position)
+        {
+            if(Cards.Count() >= position)
+                Cards.RemoveAt(position);
+        }
+
+        public Card GetCard(int position)
+        {
+            return Cards[position];
+        }
+
+
+        public void SortBySuit()
+        {
+            Cards = Cards.OrderBy(o => o.getSuit()).ToList();
+        }
+
+
+        public void sortByValue()
+        {
+            Cards = Cards.OrderBy(o => (o.getValue()+o.getSuit())).ToList();
+        }
+    }
+}
